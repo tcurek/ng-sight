8112d5f037e07ea4b0e750412753bfadf704d1ff
Tim Curek <tcurek@US-LA-ACT-01413>
2019-10-27 22:06:24 -0400

    setup pie chart

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,7 +9,18 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  public pieChartData: number[] = [350,450,120];
+  public pieChartLabels: string[] = ["Xyz Bakery","Acme","Logistics"];
+  public pieChartColors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      borderColor: '#111'
+    }
+  ];
+  public pieChartType = 'pie';
+
   ngOnInit() {
+    
   }
 
 }
