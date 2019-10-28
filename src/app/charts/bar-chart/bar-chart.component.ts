4703f3507a651424763e3d43fc3ad5d5a6133841
Tim Curek <tcurek@US-LA-ACT-01413>
2019-10-27 21:49:43 -0400

    Add chart components, applied styling, configured barchart

---
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+const SampleBarChartData: any[] = [
+  {data: [65,59,80,81,56,54,30], label: 'Q3 Sales'},
+  {data: [25,39,60,91,36,54,50], label: 'Q4 Sales'}
+];
+
+const SampleBarChartLabels: string[] = ['W1', 'W2','W3','W4','W5', 'W6', 'W7'];
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.css']
+})
+export class BarChartComponent implements OnInit {
+
+  constructor() { }
+
+  public barChartData: any[] = SampleBarChartData;
+  public barChartLabels: string[] = SampleBarChartLabels;
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartOptions: any = {
+    scaleShowVertivalLines: false,
+    responsive: true
+  }
+
+  ngOnInit() {
+  }
+
+}
