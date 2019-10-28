1a886542ded67d3dea453414b52e5018cb74149e
Tim Curek <tcurek@US-LA-ACT-01413>
2019-10-27 22:58:54 -0400

    Added line chart, styles and shared data

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { SampleLineChartColorts } from '../../shared/chart.colors'
+
+const SampleLineChartData: any[] = [
+  {data: [32,14,46,23,38,56], label: 'Sentiment Analysis'},
+  {data: [15,22,69,23,46,22], label: 'Image Recognition'},
+  {data: [22,59,64,17,64,33], label: 'Forecasting'}
+];
+
+const SampleLineChartLabels: string[] = ['Jan', 'Feb', 'Mar','Apr','May','Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +17,16 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  public lineChartData: any[] = SampleLineChartData;
+  public lineChartLabels: string[] = SampleLineChartLabels;
+  public lineChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+  public lineChartLegend = true;
+  public lineChartType = "line";
+  public lineChartColors: any[] = SampleLineChartColorts;
 
   ngOnInit() {
   }
